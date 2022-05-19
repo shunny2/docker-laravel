e4faea1e34885dc1b4ff3442c062c9c5e6999acc
Alexander Davis <alexander.davis.098@gmail.com>
2022-05-19 17:07:30 -0300

    Fix: Moving game controller to another folder

---
+++ app/Http/Controllers/API/Game/GameController.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Http\Controllers\API\Game;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreGameRequest;
+use App\Models\Game;
+use App\Models\User;
+use Exception;
+use Illuminate\Http\Request;
+
+class GameController extends Controller
+{
+    protected $game;
+
+    public function __construct(Game $game)
+    {
+        $this->game = $game;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        try {
+            $games = $this->game->paginate(15)->all();
+
+            return response()->json($games);
+        } catch (Exception $e) {
+            return response($e->getMessage(), $e->getCode());
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreGameRequest $request)
+    {
+        try {
+            $data = $request->all();
+            $this->game->create($data);
+
+            return response('Successfully registered!', '200');
+        } catch (Exception $e) {
+            return response($e->getMessage(), $e->getCode());
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        try {
+            $game = $this->game->find($id);
+
+            return response()->json($game);
+        } catch (Exception $e) {
+            return response($e->getMessage());
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        try {
+            $game = $this->game->find($id);
+
+            return response()->json($game);
+        } catch (Exception $e) {
+            return response($e->getMessage(), $e->getCode());
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        try {
+            $data = $request->all();
+            $game = $this->game->find($id);
+            $updated = $game->update($data);
+
+            return response()->json($updated);
+        } catch (Exception $e) {
+            return response($e->getMessage(), $e->getCode());
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        try {
+            $game = $this->game->find($id);
+            $game->delete();
+
+            return response()->json('Successfully deleted!', 200);
+        } catch (Exception $e) {
+            return response($e->getMessage(), $e->getCode());
+        }
+    }
+}
