27c687e20304d90da54934f64d4138cd0e8779f9
Alexander Davis <alexander.davis.098@gmail.com>
2022-05-19 17:01:55 -0300

    Fix: Added user route

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\API\GameController;
+use App\Http\Controllers\API\Game\GameController;
+use App\Http\Controllers\API\User\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,7 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::resource('user', UserController::class)->only('store', 'show');
 Route::resource('game', GameController::class);
